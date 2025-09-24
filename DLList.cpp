135499bebed5eebf9fc6e46a75dcdcc0bf85a55a
EugeniaCeinos <eceinoss@uwyo.edu>
2025-09-24 17:50:25 -0600

    Done TDL

--- DLList.cpp
+++ DLList.cpp
@@ -76,9 +76,10 @@ void DLList::traverse() {
 	else {
 		Node* curr = head;
 		while (curr != nullptr) {
-			cout << "[" << curr->data << "]" << endl;
+			cout << "[" << curr->data << "] ";
 			curr = curr->next;
 		}
+		cout << endl;
 	}
 }
 
